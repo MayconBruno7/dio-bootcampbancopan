e3984b739a903ad184b2049b7ac9d8be0395c889
Maycon <mayconbruno118@gmail.com>
2025-02-25 08:53:14 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/sintaxejava/SintaxeJava.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sintaxejava;
+
+/**
+ *
+ * @author maycon-linux
+ */
+public class SintaxeJava {
+
+    public static void main(String[] args) {
+        String meuNome = "Maycon";
+        
+        int anoFabricacao = 2002;
+        
+        boolean verdadeira = false;
+        
+        anoFabricacao = 2018;
+        
+        
+        int num1 = 1;
+        int num2 = 2;
+        
+        somar(num1, num2);
+
+    }
+    
+            
+    public static double somar(int n1, int n2) {
+        System.out.println((n1 + n2));
+        return 0;
+    }
+}
